136a9866ef7ffc04f63a9f7b1279d46f3378bd3f
Roy <shuwj81@daum.net>
2025-09-09 17:29:04 +0900

    ✨[feat]:  라이브러리 버전 수정 #2

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -5,20 +5,21 @@ import PackageDescription
     import struct ProjectDescription.PackageSettings
 
     let packageSettings = PackageSettings(
-        // Customize the product types for specific package product
-        // Default is .staticFramework
-        // productTypes: ["Alamofire": .framework,]
-        productTypes: [:]
+        productTypes: [
+
+          :
+        ]
     )
 #endif
 
 let package = Package(
-    name: "MultiModuleTemplate",
+    name: "BookSeries",
     dependencies: [
       .package(url: "http://github.com/pointfreeco/swift-composable-architecture", exact: "1.18.0"),
-      .package(url: "https://github.com/Roy-wonji/DiContainer.git", from: "1.1.4"),
-      .package(url: "https://github.com/layoutBox/FlexLayout.git", from: "2.2.2"),
-      .package(url: "https://github.com/layoutBox/PinLayout", from: "1.10.6"),
+      .package(url: "https://github.com/Roy-wonji/DiContainer.git", from: "1.1.5"),
+//      .package(url: "https://github.com/layoutBox/FlexLayout.git", from: "2.2.2"),
+//      .package(url: "https://github.com/layoutBox/PinLayout", from: "1.10.6"),
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.7.1"),
       .package(url: "https://github.com/Roy-wonji/Then", from: "2.7.0"),
       .package(url: "https://github.com/Roy-wonji/LogMacro.git", from: "1.1.0")
     ]
